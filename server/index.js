d8c662bbcb9ba30a8bd6fc34f436f085f17cb0ff
AryamanSingh <aryamansingh2803@gmail.com>
2022-03-08 17:58:18 +0530

    Deploy

--- server/index.js
+++ server/index.js
@@ -85,7 +85,6 @@ app.use('/api/dashboard/', notesRoute)
 //Serve frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname,'../client/build')))
-  
   app.get('*', (req, res) => {
     res.sendFile(
       path.resolve(__dirname, '../', 'client', 'build', 'index.html')
